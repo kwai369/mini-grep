1cd3569bc4fd297f8eb4a3d09ec20fd858f79be9
kwai369 <dhruvkalmekh369@gmail.com>
2026-09-11 07:45:31 +0530

    Instead of a subcommand token like ./build/my_clt grep [-r] 'hello' <filepath>, we made it behave more Unix-like with ./build/my_clt [-r] 'hello' <filepath>.

--- src/cli.cpp
+++ src/cli.cpp
@@ -5,33 +5,38 @@
 Config parse_arguments(int argc, char* argv[]) {
     Config config;
 
-    if (argc < 4) {
+    if (argc < 3) {
         throw std::invalid_argument("Usage: grep [-r] <pattern> <file1> [file2 ...]");
     }
 
-    if (std::string_view(argv[1]) != "grep") {
-        throw std::invalid_argument("Error: Invalid command passed");
-    }
-
-    int pattern_index = 2;
-    if (std::string_view(argv[2]) == "-r") {
-        config.recursive = true;
-        pattern_index = 3;
-
-        if (argc < 5) {
-            throw std::invalid_argument("Usage: grep [-r] <pattern> <file1> [file2 ...]");
+    int index = 1;
+    
+    while (index < argc && argv[index][0] == '-') {
+        std::string_view flag = argv[index];
+        if (flag == "-r"){
+            config.recursive = true;
         }
+        else {
+            throw std::invalid_argument("Error: Unknown flag: " + std::string(flag));
+        }
+        index++;
     }
 
-    config.pattern = argv[pattern_index];
+    if (index >= argc) {
+        throw std::invalid_argument("Error: No pattern provided");
+    }
+
+
+    config.pattern = argv[index];
     if (config.pattern.empty()) {
         throw std::invalid_argument("Error: Empty patterns are not allowed");
     }
     
     to_lowercase(config.pattern); // Standardizing input via utils
+    index++;
 
     // Collect all target paths
-    for (int i = pattern_index + 1; i < argc; i++) {
+    for (int i = index; i < argc; i++) {
         std::string path_arg = argv[i];
         if (!path_arg.empty()) {
             config.target_paths.push_back(path_arg);
